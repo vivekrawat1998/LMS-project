5cc59de584e589d63666b4d9037a1ce1285b85f2
yogesh rawat <yogeshrawatyogi345@gmail.com>
2024-02-18 17:27:14 +0530

    first mesaeg

--- frontend/src/rootEpic.js
+++
@@ -1,7 +0,0 @@
-// rootEpic.js
-import { combineEpics } from 'redux-observable';
-import getProductEpic from './getProductEpic'; // Import your epic
-
-const rootEpic = combineEpics(getProductEpic);
-
-export default rootEpic;
